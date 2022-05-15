0f21c1f294eb1cc2bdf563478f7517e0a43ce494
LittleToes <kristina.r.brennan@gmail.com>
2022-05-15 17:53:16 -0500

    fixed port issue for heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const path = require('path');
 
 const api = require('./routes/apiRoutes/apiRoutes.js');
